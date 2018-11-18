6034e98b185d1a1d60af9f221fc7879d29e263b4
= <FlorianWoelki@gmx.de>
2018-11-18 15:08:41 +0100

    Updates

---
+++ precache-manifest.b2f0bfac31ed45208a1cce1a213b3466.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "a2d04d29d5186f333476",
+    "url": "/react-todo/static/js/main.a2d04d29.chunk.js"
+  },
+  {
+    "revision": "6cc00143f9b6ebb606a5",
+    "url": "/react-todo/static/js/1.6cc00143.chunk.js"
+  },
+  {
+    "revision": "a2d04d29d5186f333476",
+    "url": "/react-todo/static/css/main.393577f8.chunk.css"
+  },
+  {
+    "revision": "c515115071b93a81358f79692057964c",
+    "url": "/react-todo/index.html"
+  }
+];
